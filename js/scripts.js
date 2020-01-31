5db8fe0177f0b7984a1b7a4e9bd992a15d3894f1
Jakob Palmer <jakempalmer@gmail.com>
2020-01-30 17:15:11 -0800

    Added img and js folders.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+var descriptors = ["Software Engineer", "Data Hoarder", "Tech Enthusiast"];
+
+
+// $(document).ready(() => {
+// 	anime({
+// 		targets: 'div.boxGroup',
+// 		translateY: [
+// 			{value: 200, duration: 1000},
+// 			{value: 100, duration: 800},
+// 			{value: 200, duration: 2500},
+// 			{value: 0, duration: 2000}, 
+// 		],
+// 		rotateX: '360deg',
+// 		loop: true,
+// 		easing: 'easeOutElastic',
+// 		width: 110,
+// 		delay: function(el, i, l) {return i * 1000}
+// 	});
+// });
+
+
+anime({targets: '#box', 
+    duration: 2000, 
+    translateX: 300,     
+});
